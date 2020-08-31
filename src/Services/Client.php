06ef7b37e6367b9d9e662449aa92c86bcaa7da04
Arushad Ahmed <dash-8x@hotmail.com>
2020-08-31 23:11:13 +0500

    - Rewritten client

--- src/Services/Client.php
+++ src/Services/Client.php
@@ -3,11 +3,62 @@
 namespace Javaabu\BmlConnect\Services;
 
 
+use BMLConnect\Transactions;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response;
+
+/**
+ * Methods from \BMLConnect\Client has been rewritten
+ * since the original class properties had private access
+ * and the endpoints had to be modified to allow support
+ * for cancel action
+ * 
+ * @package Javaabu\BmlConnect\Services
+ */
+
 class Client extends \BMLConnect\Client
 {
     // endpoints modified to support cancel operation
     const BML_SANDBOX_ENDPOINT = 'https://api.uat.merchants.bankofmaldives.com.mv/';
     const BML_PRODUCTION_ENDPOINT = 'https://api.merchants.bankofmaldives.com.mv/';
+    const BML_API_VERSION = '2.0';
+    const BML_APP_VERSION = 'bml-connect-php';
+    const BML_SIGN_METHOD = 'sha1';
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $http_client;
+
+    /**
+     * @var string
+     */
+    protected $api_key;
+
+    /**
+     * @var string
+     */
+    protected $app_id;
+
+    /**
+     * @var string
+     */
+    protected $mode;
+
+    /**
+     * @var array
+     */
+    protected $client_options;
+
+    /**
+     * @var string
+     */
+    protected $base_url;
+
+    /**
+     * @var Transactions
+     */
+    public $transactions;
 
     /**
      * Constructor
@@ -25,5 +76,132 @@ class Client extends \BMLConnect\Client
     )
     {
         parent::__construct($api_key, $app_id, $mode, $client_options);
+
+        $this->api_key = $api_key;
+        $this->app_id = $app_id;
+        $this->mode = $mode;
+        $this->base_url = ($mode === 'production' ? self::BML_PRODUCTION_ENDPOINT : self::BML_SANDBOX_ENDPOINT);
+        $this->client_options = $client_options;
+
+        $this->initHttpClient();
+
+        $this->transactions = new Transactions($this);
+    }
+
+    /**
+     * @param GuzzleClient $client
+     */
+    public function setClient(GuzzleClient $client)
+    {
+        $this->http_client = $client;
+    }
+
+    /**
+     * Initiates the HttpClient with required headers
+     */
+    protected function initHttpClient()
+    {
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' =>  $this->api_key,
+            ]
+        ];
+
+        $this->http_client = new GuzzleClient(array_replace_recursive($this->client_options, $options));
+    }
+
+    /**
+     * Build the url
+     *
+     * @param $endpoint
+     * @return string
+     */
+    protected function buildUrl($endpoint)
+    {
+        return $this->base_url . $endpoint;
+    }
+
+    /**
+     * @param Response $response
+     * @return mixed
+     */
+    protected function parseResponse(Response $response)
+    {
+        $stream = \GuzzleHttp\Psr7\stream_for($response->getBody());
+        $data = json_decode($stream);
+
+        return $data;
+    }
+
+    /**
+     * @param string $endpoint
+     * @param array $json
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function post($endpoint, $json)
+    {
+        $json['apiVersion'] = self::BML_API_VERSION;
+        $json['appVersion'] = self::BML_APP_VERSION;
+        $json['signMethod'] = self::BML_SIGN_METHOD;
+
+        $response = $this->http_client->request('POST', $this->buildUrl($endpoint), ['json' => $json]);
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * @param string $endpoint
+     * @param array $pagination
+     * @return mixed
+     */
+    public function get(string $endpoint, array $pagination = [])
+    {
+        if ($endpoint == 'transactions') {
+            $endpoint = 'public/' . $endpoint;
+        }
+
+        $response = $this->http_client->request(
+            'GET',
+            $this->setPagination($this->buildUrl($endpoint), $pagination)
+        );
+
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * @param string $url
+     * @param array $pagination
+     * @return string
+     */
+    protected function setPagination(string $url, array $pagination)
+    {
+        if (count($pagination)) {
+            return $url.'?'.http_build_query($this->sanitizePagination($pagination));
+        }
+
+        return $url;
+    }
+
+    /**
+     * @param array $pagination
+     * @return array
+     */
+    protected function sanitizePagination(array $pagination)
+    {
+        $allowed = [
+            'page',
+        ];
+
+        return array_intersect_key($pagination, array_flip($allowed));
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->api_key;
     }
 }
